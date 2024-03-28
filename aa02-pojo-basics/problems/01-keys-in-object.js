b8fe9cea9e580de4924acf2ca79ad7afa07aefb4
jayray3991 <james.r45@aol.com>
2024-03-28 19:41:57 -0400

    solved problem 1

--- aa02-pojo-basics/problems/01-keys-in-object.js
+++ aa02-pojo-basics/problems/01-keys-in-object.js
@@ -1,19 +1,25 @@
 /***********************************************************************
-Write a function called `keysInObject(obj)` that takes in an object and returns 
+Write a function called `keysInObject(obj)` that takes in an object and returns
 an array of all the keys within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.keys`.
 
 Examples:
 
+
+***********************************************************************/
 let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
 let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
-keysInObject(animals); // => ["dog", "cat", "bison"]
-keysInObject(foods); // => ["apple", "lemon", "mango"]
-***********************************************************************/
-
+console.log(keysInObject(animals)); // => ["dog", "cat", "bison"]
+console.log(keysInObject(foods)); // => ["apple", "lemon", "mango"]
 function keysInObject(obj) {
-  // Your code here 
+  let emptyArray = []
+  for (const key in obj) {
+emptyArray.push(key);
+ //console.log(obj)
+
+  }
+  return emptyArray
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
