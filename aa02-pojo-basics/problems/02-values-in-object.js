af1a2d9da6c67488ffbf0584d7f949e40b5d5c9b
jayray3991 <james.r45@aol.com>
2024-03-28 20:33:26 -0400

    changed the format

--- aa02-pojo-basics/problems/02-values-in-object.js
+++ aa02-pojo-basics/problems/02-values-in-object.js
@@ -10,19 +10,19 @@ Examples:
 
 
 ***********************************************************************/
-let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
-let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+let animals = { dog: "Wolfie", cat: "Jet", bison: "Bilbo" }
+let foods = { apple: "tart", lemon: "sour", mango: "sweet" }
 console.log(valuesInObject(animals)); // => ["Wolfie", "Jet", "Bilbo"]
 console.log(valuesInObject(foods)); // => ["tart", "sour", "sweet"]
 
 function valuesInObject(obj) {
-//   let emptyArray = []
-//   for(const key in obj){
-//  emptyArray.push(obj[key])
+  //   let emptyArray = []
+  //   for(const key in obj){
+  //  emptyArray.push(obj[key])
 
-//   }
+  //   }
 
-      return Object.values(obj)
+  return Object.values(obj)
   //emptyArray
 }
 
