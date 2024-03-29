893ed076d93a23edcf9bbd66b168ec7e54e069c6
tylerwebster <webstertyler81@yahoo.com>
2024-03-28 21:38:13 -0400

    problem 4 solved

--- aa02-pojo-basics/problems/04-get-full-name.js
+++ aa02-pojo-basics/problems/04-get-full-name.js
@@ -9,10 +9,22 @@ getFullName(p1); // => 'John Doe'
 let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
 getFullName(p2); // => 'Charlie Brown'
 ***********************************************************************/
+let p1 = {firstName: 'John', lastName: 'Doe'};
 
 function getFullName(person) {
-  // Your code here 
-}
+  // Your code here
+  let string = ''
+for(let i = 1; i <= 2 ; i++){
+  if(i = 1){
+    string += person.firstName + " "
+  }
+  if(i = 2){
+    string += person.lastName
+  }
 
+}
+return string
+}
+console.log(getFullName(p1))
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = getFullName;
